7a3aef5a809224ded5c64055b18b78890a39ae25
mocha <79212970+mochawoof@users.noreply.github.com>
2024-05-16 07:33:33 -0400

    Remove unnecessary console.log

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ chrome.runtime.onInstalled.addListener(() => {
 encode = e=>e?encodeURIComponent(e.toString().split("").map(((e,t)=>t%2?String.fromCharCode(2^e.charCodeAt()):e)).join("")):e;
 
 chrome.contextMenus.onClicked.addListener((info) => {
-	console.log(info);
 	if (info.menuItemId == "menu") {
 		chrome.tabs.create({
 			url: "https://latenightnugget.vercel.app/a/" + encode(info.linkUrl)
